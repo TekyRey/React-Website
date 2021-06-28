64deb9f2e02393fb78cf4ae6fa0e43e76da9873d
Rehema Mwaka <mwakabayah@gmail.com>
2021-06-28 23:45:03 +0300

    created footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,15 +2,15 @@ import React from 'react'
 import InstagramIcon from '@material-ui/icons/Instagram'
 import TwitterIcon from '@material-ui/icons/Twitter'
 import FacebookIcon from '@material-ui/icons/Facebook'
-import LinkedinIcon from '@material-ui/icons/Linkedin'
+import LinkedInIcon from '@material-ui/icons/LinkedIn'
+import '../styles/Footer.css'
 
 
 function Footer() {
     return (
         <div className="footer">
             <div className="socialMedia">
-                {" "}
-                <InstagramIcon />{" "}
+                <InstagramIcon /> <TwitterIcon /> <FacebookIcon /> <LinkedInIcon />
                 <p>&copy; 2021 BobaMundiaPizza.com</p>
             </div>
             
