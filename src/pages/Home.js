3892031bd1ee4314c821057fd558bf53f0ba8fdf
Rehema Mwaka <mwakabayah@gmail.com>
2021-06-28 23:22:11 +0300

    created top navbar

---
+++ src/pages/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import BannerImage from '../assets/pizza.jpeg'
+
+function Home() {
+    return (
+        <div className="home">
+            <div className="headerContainer" style={{ backgroundImage: `url(${BannerImage})` }}>
+                <h1>Boba's Pizzeria</h1>
+                <p>PIZZA TO FIT ANY TASTE</p>
+                <Link to="/menu">
+                    <button>ORDER NOW</button>
+                </Link>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Home
